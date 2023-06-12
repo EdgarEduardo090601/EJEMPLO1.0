d4e50809543160ef47b6cfcf8d54099fc4d06cc9
EDGAREDUARDOCARRILLOCRUZ <eduard999m@hotmail.com>
2023-06-12 12:18:18 -0600

    ASI COLOQUE CUAL ES EL DE DENISSE

--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub BTN1_Click(sender As Object, e As EventArgs) Handles BTN1.Click
-        txt1.Text = "HOLA, SOY REBECA"
+        txt1.Text = "este es el de denisse"
     End Sub
 End Class
